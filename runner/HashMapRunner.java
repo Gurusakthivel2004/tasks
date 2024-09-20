29f3a54fb2baacf38b401bcdd1beca946e3ac133
Gurusakthivel2004 <gurusakthivel.v2021ecea@sece.ac.in>
2024-09-20 15:18:15 +0530

    Sep 20 15:19

--- runner/HashMapRunner.java
+++ runner/HashMapRunner.java
@@ -3,6 +3,7 @@ package runner;
 import tasks.HashMapTask;
 import exception.CustomException;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -60,9 +61,53 @@ public class HashMapRunner {
         	break;
         	
         case 8:
-        	handleContainsKey();
+        	handleContainsKey(reader);
         	break;
         	
+        case 9:
+        	handleContainsValue(reader);
+        	break;
+        	
+        case 10:
+        	handleReplaceValues(reader);
+        	break;
+        	
+        case 11:
+        	handleExistingKey(reader);
+        	break;
+        	
+        case 12:
+        	handleNonExistingKey(reader);
+        	break;
+        	
+        case 13:
+        	handleDefaultValue(reader);
+        	break;
+        	
+        case 14:
+        	handleRemoveKey(reader);
+        	break;
+        	
+        case 15:
+        	handleRemoveKeyValue(reader);
+        	break;
+        
+        case 16:
+        	handleReplaceValue(reader);
+        	break;
+        	
+        case 17:
+        	handleReplaceValueIfMatches(reader);
+        	break;
+        	
+        case 18:
+        	handleTransfer(reader);
+        	break;
+        	
+        case 19:
+        	handleIterator(reader);
+        	break;
+        		
         case -1:
         	hmRunner.flag = false;
         	return;
@@ -70,10 +115,114 @@ public class HashMapRunner {
     }
   }
   
-  public void handleContainsKey() throws CustomException {
-	  Map<String, String> map = hmtask.getHashMap();
-	  hmtask.add(map, "leo", "goat");
-	  System.out.println("Does map contains the key leo ? " + hmtask.containsKey(map, "leo"));
+  
+  
+  public void handleTransfer(Scanner reader) throws CustomException {
+	  Map<String, String> firstMap = handleGetStrings(reader);
+	  Map<String, String> secondMap = handleGetStrings(reader);
+	  hmtask.putAll(firstMap, secondMap);
+	  printMap(firstMap);
+	  printMap(secondMap);
+  }
+  
+  public void handleReplaceValueIfMatches(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter the key : ");
+	  String key = reader.next();
+	  System.out.println("Enter the old value for that key : ");
+	  String oldValue = reader.next();
+	  System.out.println("Enter the new value that you want to replace : ");
+	  String newValue = reader.next();
+	  hmtask.replace(map, key, oldValue, newValue);
+	  printMap(map);
+  }
+  
+  public void handleReplaceValue(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter the key : ");
+	  String key = reader.next();
+	  System.out.println("Enter the new value that you want to replace : ");
+	  String newValue = reader.next();
+	  hmtask.replace(map, key, newValue);
+	  printMap(map);
+  }
+  
+  public void handleRemoveKeyValue(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter a existing key : ");
+	  String key = reader.next();
+	  System.out.print("Enter it's value : ");
+	  String value = reader.next();
+	  hmtask.remove(map, key, value);
+	  printMap(map);
+  }
+  
+  public void handleRemoveKey(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter a existing key : ");
+	  String key = reader.next();
+	  hmtask.remove(map, key);
+	  printMap(map);
+  }
+  
+  
+  public void handleDefaultValue(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter a non existing key : ");
+	  String key = reader.next();
+	  String value = hmtask.getOrDefault(map, key, "zoho");
+	  System.out.println("The value is " + value);
+  }
+  
+  public void handleNonExistingKey(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter a non existing key : ");
+	  String key = reader.next();
+	  String value = hmtask.get(map, key);
+	  System.out.println("The value is " + value);
+  }
+  
+  public void handleExistingKey(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter the key that you want to get the value from : ");
+	  String key = reader.next();
+	  String value = hmtask.get(map, key);
+	  System.out.println("The value is " + value);
+  }
+  
+  public void handleReplaceValues(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  int mapSize = hmtask.getMapLength(map);
+	  while(mapSize --> 0) {
+		  System.out.print("Enter the key : ");
+		  String key = reader.next();
+		  System.out.println("Enter the new value that you want to replace : ");
+		  String newValue = reader.next();
+		  hmtask.replace(map, key, newValue);
+	  }
+	  printMap(map);
+  }
+  
+  public void handleContainsValue(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter the value that you want to check : ");
+	  String value = reader.next();
+	  System.out.println("Does map contains the key leo ? " + hmtask.containsValue(map, value));
+  }
+  
+  public void handleContainsKey(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  System.out.print("Enter the key that you want to check : ");
+	  String key = reader.next();
+	  System.out.println("Does map contains the key leo ? " + hmtask.containsKey(map, key));
+  }
+  
+  public void handleIterator(Scanner reader) throws CustomException {
+	  Map<String, String> map = handleGetStrings(reader);
+	  Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator(); 
+      while(itr.hasNext()) { 
+    	System.out.println("key : " + itr.next().getKey() + ", value : " + itr.next().getValue()); 
+      } 
   }
   
   public Map<String, String> handleNullKey() throws CustomException {
--- tasks/HashMapTask.java
+++ tasks/HashMapTask.java
@@ -18,9 +18,56 @@ public class HashMapTask  {
 		return map.containsKey(key);
 	}
 	
+	public <K, V> V remove(Map<K, V> map, K key) throws CustomException {
+		Helper.checkNullValues(map);
+		Helper.checkNullValues(key);
+		return map.remove(key);
+	}
+	
+	public <K, V> boolean remove(Map<K, V> map, K key, V value) throws CustomException {
+		Helper.checkNullValues(map);
+		Helper.checkNullValues(key);
+		return map.remove(key, value);
+	}
+	
+	public <K, V> boolean containsValue(Map<K, V> map, V value) throws CustomException {
+		Helper.checkNullValues(map);
+		Helper.checkNullValues(value);
+		return map.containsValue(value);
+	}
+	
+	public <K, V> Map<K, V> replace(Map<K, V> map, K key, V value) throws CustomException {
+		Helper.checkNullValues(map);
+		Helper.checkNullValues(key);
+		Helper.checkNullValues(value);
+		map.replace(key, value);
+		return map;
+	}
+	
+	public <K, V> void putAll(Map<K, V> firstMap, Map<K, V> secondMap) throws CustomException {
+		Helper.checkNullValues(firstMap);
+		Helper.checkNullValues(secondMap);
+		firstMap.putAll(secondMap);
+	}
+	
+	public <K, V> Map<K, V> replace (Map<K, V> map, K key, V oldValue, V newValue) throws CustomException {
+		Helper.checkNullValues(map);
+		Helper.checkNullValues(key);
+		Helper.checkNullValues(oldValue);
+		Helper.checkNullValues(newValue);
+		map.replace(key, oldValue, newValue);
+		return map;
+	}
+	
 	public <K, V> int getMapLength(Map<K, V> map) throws CustomException {
 		Helper.checkNullValues(map);
 		return map.size();
+	}
+	
+	public <K, V> V getOrDefault(Map<K, V> map, K key, V defaultValue) throws CustomException {
+		Helper.checkNullValues(map);
+		Helper.checkNullValues(key);
+		return map.getOrDefault(key, defaultValue);
 	}
 	
 	public <K, V> V get(Map<K, V> map, K key) throws CustomException {
